7e3c09141d69b707d60bfb67f6237051e4c0451d
coneale90 <coneale90@hotmail.it>
2022-09-25 13:19:02 +0200

    Added main

---
+++ main.py
@@ -0,0 +1,39 @@
+import logging
+
+from jproperties import Properties
+
+from app.main_service import MainService
+from app.repositoriy.postgres_db import Postgres
+
+
+def __read_db_properties__() -> Properties:
+    configs = Properties()
+    with open('db.properties', 'rb') as read_prop:
+        configs.load(read_prop)
+    return configs
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        filename='/home/pi/reader.log',
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        filemode='a',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    file1 = open('/home/pi/serial_number', 'r')
+    lines = file1.readlines()
+    raspberry_identifier = lines[0].rstrip('\x00').lstrip('\x00')
+    logging.info("raspberry_identifier: " + raspberry_identifier)
+    db_configs = __read_db_properties__()
+    db = Postgres(
+        db_url=db_configs.get('DB_URL'),
+        db_user=db_configs.get('DB_USER'),
+        db_password=db_configs.get('DB_PASSWORD'),
+        db_name=db_configs.get('DB_NAME'),
+        db_schema=db_configs.get('DB_SCHEMA')
+    )
+    ms = MainService(raspberry_identifier, db)
+    ms.start_reading_sensors()
+
+
